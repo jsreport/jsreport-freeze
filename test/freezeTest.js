aacf279e0d02f66ec86827950ad6832dee9c738f
BJR Matos <bjrmatos@gmail.com>
2018-04-06 13:16:39 -0500

    collection not longer have .name property

--- test/freezeTest.js
+++ test/freezeTest.js
@@ -12,32 +12,32 @@ describe('freeze', () => {
   })
 
   it('should allow iserts in default', async () => {
-    return reporter.documentStore.collection('templates').insert({ name: 'foo' })
+    return reporter.documentStore.collection('templates').insert({ name: 'foo', engine: 'none', recipe: 'html' })
   })
 
   it('should allow update in default', async () => {
-    await reporter.documentStore.collection('templates').insert({ name: 'foo' })
+    await reporter.documentStore.collection('templates').insert({ name: 'foo', engine: 'none', recipe: 'html' })
     return reporter.documentStore.collection('templates').update({ name: 'foo' }, { $set: { content: 'foo ' } })
   })
 
   it('should allow remove in default', async () => {
-    await reporter.documentStore.collection('templates').insert({ name: 'foo' })
+    await reporter.documentStore.collection('templates').insert({ name: 'foo', engine: 'none', recipe: 'html' })
     return reporter.documentStore.collection('templates').remove({ name: 'foo' })
   })
 
   it('should block iserts in freeze', async () => {
     await reporter.settings.addOrSet('freeze', true)
-    return reporter.documentStore.collection('templates').insert({ name: 'foo' }).should.be.rejectedWith(/frozen/)
+    return reporter.documentStore.collection('templates').insert({ name: 'foo', engine: 'none', recipe: 'html' }).should.be.rejectedWith(/frozen/)
   })
 
   it('should block update in freeze', async () => {
-    await reporter.documentStore.collection('templates').insert({ name: 'foo' })
+    await reporter.documentStore.collection('templates').insert({ name: 'foo', engine: 'none', recipe: 'html' })
     await reporter.settings.addOrSet('freeze', true)
     return reporter.documentStore.collection('templates').update({ name: 'foo' }, { $set: { content: 'foo' } }).should.be.rejectedWith(/frozen/)
   })
 
   it('should block remove in freeze', async () => {
-    await reporter.documentStore.collection('templates').insert({ name: 'foo' })
+    await reporter.documentStore.collection('templates').insert({ name: 'foo', engine: 'none', recipe: 'html' })
     await reporter.settings.addOrSet('freeze', true)
     return reporter.documentStore.collection('templates').remove({ name: 'foo' }).should.be.rejectedWith(/frozen/)
   })
@@ -54,6 +54,6 @@ describe('freeze with hardFreeze', () => {
   })
 
   it('should block insert', async () => {
-    return reporter.documentStore.collection('templates').insert({ name: 'foo' }).should.be.rejectedWith(/frozen/)
+    return reporter.documentStore.collection('templates').insert({ name: 'foo', engine: 'none', recipe: 'html' }).should.be.rejectedWith(/frozen/)
   })
 })
